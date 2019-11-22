b782024504375f684a8221df4b4b1b1f4dbf58f9
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-22 13:32:44 +0100

    LogOut button implementing and deleting NavigationBarBackButton

--- ChatAppWithFirebase/Controller/ChatViewController.swift
+++ ChatAppWithFirebase/Controller/ChatViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutPressed(_ sender: Any) {
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
     }
-    */
-
 }
