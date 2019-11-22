990dd9aa7eb7a9788c15048e43110cbbd11ff85c
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-22 13:18:20 +0100

    Login and Register Authorisation button

--- ChatAppWithFirebase/Controller/LoginViewController.swift
+++ ChatAppWithFirebase/Controller/LoginViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    self?.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
-    */
-
 }
