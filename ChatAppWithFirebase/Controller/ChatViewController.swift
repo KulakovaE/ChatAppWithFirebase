736050ee3f81aaac79c34797155ef2d271ab715a
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-23 22:23:44 +0100

    Working on MessageCell

--- ChatAppWithFirebase/Controller/ChatViewController.swift
+++ ChatAppWithFirebase/Controller/ChatViewController.swift
@@ -14,11 +14,18 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
@@ -28,5 +35,19 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print ("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        if let cell = cell as? MessageCell {
+            cell.messageLabel.text = messages[indexPath.row].body
+        }
+        return cell
     }
 }
---
+++ ChatAppWithFirebase/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  ChatAppWithFirebase
+//
+//  Created by Darko Kulakov on 2019-11-23.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //email
+    let body: String //the body of the message
+}
